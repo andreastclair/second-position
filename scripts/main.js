bc3099e8d05459d4eea4014f597264634be32c57
Andrea St. Clair <andreadoris86@gmail.com>
2015-08-09 00:05:14 -0500

    added modal to item pages

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,42 @@ $(document).ready(function(){
 			$(this).attr('src', 'images/favorite.png');
 		}
 	})
+
+	// SIZE CHART
+
+var show = $('.show-more');
+var hide = $('.hide-modal');
+var backdrop = $('.modal-backdrop');
+
+show.click(function(){
+	showMore();
 });
 
+hide.click(function(){
+	hideModal();
+})
 
+function hideModal() {
+	backdrop.hide();
+}
+
+$('img').click(function(){
+  $(this).addClass('hide-img');
+})
+
+hide.click(function(){
+  $('img').removeClass('hide-img');
+})
+
+function showMore() {
+	backdrop.show();
+}
+
+function showText() {
+  show.html('show more');
+}
+
+function hideText() {
+  show.html('hide more');
+}
+});
